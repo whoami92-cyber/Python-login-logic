66913ccefdff5cd114ea2b569700820eccae807d
whoami92-cyber <niko.iijolainen@outlook.com>
2026-03-29 18:48:31 +0300

    update

--- user.py
+++ user.py
@@ -1,16 +1,24 @@
 import time
 import getpass
-attempst=0
+
+attemps=0
 while True:
     user=input("User: ")
     pw=getpass.getpass("Password: ")
-    if user =='admin' and pw =='1234':
+    if  user =='admin' and pw =='1234':
+        time.sleep(1)
         print("Hello")
+        a= ['A', 'AB', 'CD', 'EF']
+        print (a)
+        time.sleep(1)
+        print("bye")
         break
     else:
        print("Error")
-       attempst  +=1
+       attemps  +=1
+       time.sleep(1)
+       print ("attemps", attemps)
        time.sleep(2)
-    if attempst == 3:
+    if attemps == 3:
        print("wrong user or password")
        break
